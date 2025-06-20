c1011bcc248fa92f7d833fdff75924901a5ce167
Marc Alcaraz <ekameleon@gmail.com>
2025-06-20 18:32:00 +0200

    test color

--- src/oihana/logging/monolog/processors/SymbolProcessor.php
+++ src/oihana/logging/monolog/processors/SymbolProcessor.php
@@ -7,6 +7,17 @@ use Monolog\LogRecord;
 
 class SymbolProcessor
 {
+    /**
+     * Creates a new SymbolProcessor.
+     * @param bool $useColor Indicates if the level symbol use colors.
+     */
+    public function __construct( bool $useColor = true )
+    {
+        $this->useColor = $useColor ;
+    }
+
+    public bool $useColor ;
+
     private array $symbolMap =
     [
         Level::Debug->value     => '›' , // 100
@@ -19,6 +30,18 @@ class SymbolProcessor
         Level::Emergency->value => '☢' , // 600
     ];
 
+    private array $colorMap =
+    [
+        Level::Debug->value     => "\033[37m",    // Gris
+        Level::Info->value      => "\033[32m",    // Vert
+        Level::Notice->value    => "\033[36m",    // Cyan
+        Level::Warning->value   => "\033[33m",    // Jaune
+        Level::Error->value     => "\033[31m",    // Rouge
+        Level::Critical->value  => "\033[35m",    // Magenta
+        Level::Alert->value     => "\033[91m",    // Rouge clair
+        Level::Emergency->value => "\033[37;41m", // Blanc sur fond rouge
+    ];
+
     /**
      * Invoke the processor.
      * @param LogRecord $record The log record reference.
@@ -28,13 +51,13 @@ class SymbolProcessor
     {
         $logLevelValue = $record['level'] ?? Level::Debug->value;
 
-        if( isset( $this->symbolMap[ $logLevelValue ] ) )
+        $emoji = $this->symbolMap[ $logLevelValue ] ?? $record['level_name'] ?? '';
+
+        if ( $this->useColors && isset( $this->colorMap[ $logLevelValue ] ) )
         {
-            $emoji = $this->symbolMap[ $logLevelValue ] ;
-        }
-        else
-        {
-            $emoji = $record['level_name'] ?? '' ;
+            $colorStart = $this->colorMap[$logLevelValue] ?? "\033[0m";
+            $colorEnd = "\033[0m"; // Réinitialiser la couleur
+            $emoji = $colorStart . $emoji . $colorEnd;
         }
 
         $record['extra']['level_emoji'] = $emoji;
