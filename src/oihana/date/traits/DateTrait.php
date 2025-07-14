11207419798bb7cbec9b6eb0416661b4b8a0e8bc
Marc Alcaraz <ekameleon@gmail.com>
2025-07-14 19:12:08 +0200

    Update DateTrait.php

--- src/oihana/date/traits/DateTrait.php
+++ src/oihana/date/traits/DateTrait.php
@@ -2,13 +2,6 @@
 
 namespace oihana\date\traits;
 
-use DateInvalidTimeZoneException;
-use DateMalformedStringException;
-use DateTime;
-use DateTimeZone;
-
-use function oihana\core\date\isDate;
-
 /**
  * The command to manage an ArangoDB database.
  */
@@ -35,22 +28,4 @@ trait DateTrait
      * @var ?string
      */
     public ?string $timezone = 'Europe/Paris' ;
-
-    /**
-     * Returns a timestamped string
-     * @param string|null $date
-     * @param string|null $timezone
-     * @param string|null $format
-     * @return string|null
-     * @throws DateInvalidTimeZoneException
-     * @throws DateMalformedStringException
-     */
-    public function getTimestampedString( ?string $date = null , ?string $timezone = null , ?string $format = null ): ?string
-    {
-        $format    = $format ?? $this->dateFormat ;
-        $date      = isDate( $date , $format ) ? $date : self::NOW ;
-        $timezone  = new DateTimeZone( $timezone ?? $this->timezone ) ;
-        $dateTime  = new DateTime( $date , $timezone ) ;
-        return $dateTime->format( $format ) ;
-    }
 }
