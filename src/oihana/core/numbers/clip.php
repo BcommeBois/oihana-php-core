8cf878af685d5a1556a907bd90d9e138ad593a0b
Marc Alcaraz <ekameleon@gmail.com>
2025-10-18 12:01:53 +0200

    Update clip.php

--- src/oihana/core/numbers/clip.php
+++ src/oihana/core/numbers/clip.php
@@ -5,10 +5,10 @@ namespace oihana\core\numbers ;
 /**
  * Bounds a number value between two numbers.
  *
- * @param float $value The value to clamp.
- * @param float $min The minimum value of the range.
- * @param float $max The maximum value of the range.
- * @return float A value bounded between $min and $max.
+ * @param int|float $value The value to clamp.
+ * @param int|float $min The minimum value of the range.
+ * @param int|float $max The maximum value of the range.
+ * @return int|float A value bounded between $min and $max.
  *
  * @example
  * ```php
@@ -21,7 +21,7 @@ namespace oihana\core\numbers ;
  * @author  Marc Alcaraz (ekameleon)
  * @since   1.0.0
  */
-function clip( float $value , float $min , float $max ) :float
+function clip( int|float $value , int|float $min , int|float $max ) :int|float
 {
     if ( $value <= $min )
     {
